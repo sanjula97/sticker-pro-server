42ce282a0af29bb052a492e90b3b7dc48f13359a
Sanjula Karunarathna <sanjulakarunarathna@Sanjulas-MacBook-Pro.local>
2022-08-05 01:18:51 +0530

    project setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import data from '../data.js';
+import bodyParser from 'body-parser'
 
 const app = express();
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -13,6 +17,11 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
   // res.send('Hello World! babel')
 });
+
+app.post('/api/register', (req, res) => {
+  console.log("req body", req.body)
+  res.json({ status: 'ok'})
+})
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {

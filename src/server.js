0f909c5a6dc6a48b20a84522ace67f5d1730c0b4
Sanjula Karunarathna <sanjulakarunarathna@Sanjulas-MacBook-Pro.local>
2022-08-10 12:49:47 +0530

    add password reset feature

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import cors from 'cors';
 import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 import data from '../data.js';
 import User from '../models/user'
+
+const JWT_SECRET = 'hwkghkjhwr7667%^$gadhj'
 
 console.log("User model =>", User)
 
@@ -24,14 +27,44 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }))
 
-// test
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-  // res.send('Hello World! babel')
+//Login
+app.post('/api/login', async (req, res) => {
+  const {username, password} = req.body
+  const user = await User.findOne({username}).lean()
+
+  if(!user){
+    return res.send({status: 'error', data: {token: 'Invalid username/password'}});
+  }
+
+  if(await bcrypt.compare(password, user.password)){
+
+    const token = jwt.sign({
+      id: user._id,
+      username: user.username
+    }, JWT_SECRET)
+
+    return res.send({status: 'ok', data: {token, message: 'Valid credentials'}});
+  }
+
+  res.send({status: 'error', data: {message: 'Invalid username/password'}});
 });
 
+
+//Register
 app.post('/api/register', async (req, res) => {
   const {username, password} = req.body
+
+  if(!username || typeof username !== 'string') {
+    return res.json({ status: 'error', message: 'Invalid username'})
+  }
+
+  if(!password || typeof password !== 'string') {
+    return res.json({ status: 'error', message: 'Invalid password'})
+  }
+
+  if(password.length < 5) {
+    return res.json({ status: 'error', message: 'Password too small. Should be atleast 6 charactors'})
+  }
 
   var hashedPassword =  await bcrypt.hash(password, 10);
   console.log("hashed body body",  hashedPassword)
@@ -49,10 +82,43 @@ app.post('/api/register', async (req, res) => {
       return res.json({ status: 'error', message: 'Username already in use'})
     }
     throw error
-    
   }
 
 })
+
+//change password
+app.post('/api/change-password', async (req, res) => {
+  const {token, newPassword} = req.body
+
+  if(!newPassword || typeof newPassword !== 'string') {
+    return res.json({ status: 'error', message: 'Invalid password'})
+  }
+
+  if(newPassword.length < 5) {
+    return res.json({ status: 'error', message: 'Password too small. Should be atleast 6 charactors'})
+  } 
+
+  try {
+    const user = jwt.verify(token, JWT_SECRET)
+    const _id = user.id
+    const hashedPassword = await bcrypt.hash(newPassword, 10)
+    await User.updateOne(
+        {_id},
+        {
+          $set: {password: hashedPassword}
+        }
+      )
+    res.send({status: 'ok', data: {message: 'Password changed'}});
+  } catch (error) {
+    res.send({status: 'error', data: {message: 'Server error'}});
+  }
+
+});
+
+//Get Products
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
 
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
